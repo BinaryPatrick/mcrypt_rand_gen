f8d5c5e3d6dd46c807ff886a1a3ac6307ae828d7
Patrick Moore <BinaryPatrick@users.noreply.github.com>
2015-04-20 15:52:55 -0400

    Update imagetest.php

--- imagetest.php
+++ imagetest.php
@@ -1,16 +1,22 @@
-<?php include('mcrypt_rand_gen.php');
+<?php if (!isset($_GET['num'])) { ?>
+	<form method="get">
+		<input type="number" name="num" placeholder="Picture Size" required>
+		<button type="submit">Submit</button>
+	</form>
+<?php } else {
+	include('function.php');
 	ini_set('memory_limit', '-1');
 	header('Content-Type: image/png');
-	$lenght = 854;
-	$im = imagecreatetruecolor($lenght, $lenght);
+	$length = $_GET['num'];
+	$im = imagecreatetruecolor($length, $length);
 	$blue = imagecolorallocate($im, 0, 255, 255);
-	for ($y = 0; $y < $lenght; $y++) {
-	    for ($x = 0; $x < $lenght; $x++) {
-	        if (mcrypt_rand_gen(0,1) == 0) {
-	            imagesetpixel($im, $x, $y, $blue);
-	        }
-	    }
-	}       
+	for ($y = 0; $y < $length; $y++) {
+		for ($x = 0; $x < $length; $x++) {
+			if (mcrypt_rand_gen(0,1) == 0) {
+				imagesetpixel($im, $x, $y, $blue);
+			}
+		}
+	}
 	imagepng($im);
 	imagedestroy($im);
-?>
+} ?>
